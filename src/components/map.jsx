d5dda84fef8920e9fb37593fb2c0cc95da945ee6
alex sanchez <alexjsanchez1020@gmail.com>
2024-10-12 15:13:28 -0400

    map visable

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import L from 'leaflet'; // Ensure leaflet is installed: `npm install leaflet`
+import 'leaflet/dist/leaflet.css'; // Leaflet CSS is required
 
-function map() {
+function Map() {
+    useEffect(() => {
+        // Initialize the map only once
+        const map = L.map('map', {
+            center: [51.505, -0.09],
+            zoom: 13,
+        });
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        }).addTo(map);
+
+        return () => {
+            map.remove(); // Cleanup on component unmount to prevent memory leaks
+        };
+    }, []);
+
     return (
-        <div>
-            <div id="map"></div>
-        </div>
-    )
+        <div style={{ height: '400px', width: '100%' }} id="map"></div>
+    );
 }
 
-export default map
+export default Map;
